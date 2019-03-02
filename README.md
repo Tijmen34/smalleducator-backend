0e5658bf666eaa095734dae200c5c4d34a771b96
Tijmen Stor <tm.stor@outlook.com>
2019-03-02 13:39:37 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## SmallEducator backend
+
+This Java Spring Boot project is a backend for the SmallEducator project.
---
+++ src/main/java/com/hva/aad/SmallEducator/SmallEducatorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hva.aad.SmallEducator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallEducatorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallEducatorBackendApplication.class, args);
+	}
+
+}
